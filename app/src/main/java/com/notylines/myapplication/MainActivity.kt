4572747d0337f161ad1ff5e2249c668b05ad5d02
Esteban <cepalacios@outlook.com>
2024-07-21 14:11:05 -0500

    Refactor splash screen theme and icon size

--- app/src/main/java/com/notylines/myapplication/MainActivity.kt
+++ app/src/main/java/com/notylines/myapplication/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.notylines.myapplication
 
 import android.os.Bundle
-import android.window.SplashScreen
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
-        Thread.sleep(2000)
+        installSplashScreen().apply {
+            Thread.sleep(2000)
+        }
         setContentView(R.layout.activity_main)
     }
 }
